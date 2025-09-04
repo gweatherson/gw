17f175873fa5461a2f586b3c6af646623e53e72c
Glenn Weatherson <gweatherson@gmail.com>
2025-09-04 16:17:24 -0700

    clean up

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,6 +4,7 @@ import { glob, file } from 'astro/loaders';
 
 const goods = defineCollection({
   schema: z.object({
+    date: z.string().transform((date) => new Date(date)),
     title: z.string(),
   }),
 });
