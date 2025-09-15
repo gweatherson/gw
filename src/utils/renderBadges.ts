5d72a3b8722eef93720182f73d0637b70e2c9eee
Glenn Weatherson <gweatherson@gmail.com>
2025-09-14 20:48:21 -0700

    style for sub pages

--- src/utils/renderBadges.ts
+++ src/utils/renderBadges.ts
@@ -1,5 +1,12 @@
 export function renderBadges(html: string): string {
+  html = decodeHTMLEntities(html);
   html = html.replace(/#(\w+)/g, '<span class="badge badge-xs">#$1</span> ');
   html = html.replace(/((<span class="badge badge-xs">#\w+<\/span>\s*)+)(?=<a )/g, (match) => match + '<br>');
   return html;
+}
+
+function decodeHTMLEntities(text: string): string {
+  return text.replace(/&#([0-9]+);/g, (match, dec) => String.fromCharCode(dec))
+    .replace(/&#x([0-9a-fA-F]+);/g, (match, hex) => String.fromCharCode(parseInt(hex, 16)))
+    .replace(/&amp;/g, '&');
 }
